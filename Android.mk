5e917d4ef6d667dec0c049aa63e22d44317e512a
Tuan Anh <tuan73176@gmail.com>
2025-01-25 22:18:44 +0700

    DDV: Extract proprietary firmwares

--- Android.mk
+++ Android.mk
@@ -3,3 +3,20 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),DDV)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,b8e2ab5e5c2871eea6b1b112a4f01f6051b8aaad)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,07a9fd0c398211817e94516449914ee1e743c38d)
+$(call add-radio-file-sha1-checked,radio/cmnlib.img,115115babcbf60d4a6dc7721690d8ac1824b4998)
+$(call add-radio-file-sha1-checked,radio/cmnlib64.img,52a1757d8ce541e76722edd2a103914733c528f7)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,88c79d9056c39dc7518758e9a39e4ead031731ba)
+$(call add-radio-file-sha1-checked,radio/hyp.img,47c573ba0717076de7d2718fa9a665299672c7e1)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,d485abe77e2db0b6b0a9dc40ff7610be54eb45dd)
+$(call add-radio-file-sha1-checked,radio/modem.img,31b02e7c26b54d849b3cc8138eea11f9a2d162fd)
+$(call add-radio-file-sha1-checked,radio/pmic.img,85d99c109d98774b861626ab069f8fac432236c9)
+$(call add-radio-file-sha1-checked,radio/rpm.img,d82bc2d25792e7215fca4f8f0c7dd3eb61b9da85)
+$(call add-radio-file-sha1-checked,radio/tz.img,d1f623eebd4c978bd8684f9418e633b39c9df950)
+$(call add-radio-file-sha1-checked,radio/xbl.img,9341cf4626e0d0544fb4cfc96da47e63cb5fa7f5)
+
+endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,17 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    abl \
+    bluetooth \
+    cmnlib \
+    cmnlib64 \
+    devcfg \
+    hyp \
+    keymaster \
+    modem \
+    pmic \
+    rpm \
+    tz \
+    xbl
